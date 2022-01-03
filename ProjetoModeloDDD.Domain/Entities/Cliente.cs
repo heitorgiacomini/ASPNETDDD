2d3f62c4fc66cdf475d061f4468249fdb3308516
Notebook Heitor <heithor_thebest@hotmail.com>
2022-01-03 15:56:26 -0300

    first commit

---
+++ ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        [Key]
+        public long Cliente_Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloDDDContext.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModeloDDDContext : DbContext
+    {
+        public ProjetoModeloDDDContext()
+            : base("ProjetoModeloDDD")
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+    }
+}
